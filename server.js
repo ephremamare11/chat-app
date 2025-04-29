040ea920b0650a6bfbd6ff5342e04ce2b1513d50
Ephrem Amare <ephremamare@Ephrems-MacBook-Pro.local>
2025-04-29 19:35:16 -0400

    Final working chat app

--- server.js
+++ server.js
@@ -35,3 +35,7 @@ io.on('connection', (socket) => {
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Chat app server is working!');
+});
